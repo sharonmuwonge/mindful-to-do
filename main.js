69872e72d44de39d9f9bda3524dcf91d9ef43659
shamuw <64615640+shamuw@users.noreply.github.com>
2022-04-23 08:49:13 -0700

    Add random gradient background picker and close task at button click and refresh

--- main.js
+++ main.js
@@ -1,21 +1,40 @@
-let apiKey
-let projectID
+document.addEventListener('DOMContentLoaded', getBackground(), getTask())
 
-if (!localStorage.getItem('apiKey')) {
-    apiKey = prompt('Please enter your Todoist API token')
-    localStorage.setItem('apiKey', apiKey);
-}
+function getBackground() {
 
-if (!localStorage.getItem('projectID')) {
-    projectID = prompt('Please provide the ID of your single item project')
-    localStorage.setItem('projectID', projectID);   
-}
-
-if (localStorage.getItem('apiKey') && localStorage.getItem('projectID')) {
-    getTask(apiKey, projectID)
+    let main = document.querySelector('main')
+      
+      fetch('https://www.colr.org/json/schemes/random/7?scheme_size_limit=>5', {cache: "no-cache"})
+        .then(response => response.json())
+        .then(result => {
+            console.log(result)
+            console.log(result.schemes)
+            if (result.schemes.length > 0) {
+                let colors = result.schemes[0].colors
+                main.style.background = `linear-gradient(90deg, #${colors[1]}, #${colors[3]})`
+                console.log(colors)
+            } else {
+                main.style.background = `linear-gradient(90deg, #4b6cb7, #182848)`
+            }
+        })
+        .catch(error => console.log('error', error));
 }
 
 function getTask() {
+
+    let apiKey
+    let projectID
+
+    if (!localStorage.getItem('apiKey')) {
+        apiKey = prompt('Please enter your Todoist API token')
+        localStorage.setItem('apiKey', apiKey);
+    }
+
+    if (!localStorage.getItem('projectID')) {
+        projectID = prompt('Please provide the ID of your single item project')
+        localStorage.setItem('projectID', projectID);   
+    }
+
     var myHeaders = new Headers();
     myHeaders.append(`Authorization`, `Bearer ${localStorage.apiKey}`);
 
@@ -34,12 +53,47 @@ function getTask() {
         if (todo) {
             document.querySelector('h1').innerText = todo.content
             document.querySelector('p').innerText = todo.description
+            document.querySelector('#done').innerText = 'done!'
+
+            let doneButton = document.querySelector('#done')
+            doneButton.addEventListener('click', closeTaskAndRefresh)
+            
+            localStorage.taskID = todo.id
+            
         } else {
             document.querySelector('h1').innerText = 'Done! Great job.'
+            document.querySelector('#done').innerText = 'I\'\m ready for something else'
         }
     })
     .catch(err => {
         console.log(`error ${err}`)
         });
+}
+
+function closeTaskAndRefresh() {
+
+    let apiKey
+
+    if (!localStorage.getItem('apiKey')) {
+        apiKey = prompt('Please enter your Todoist API token')
+        localStorage.setItem('apiKey', apiKey);
+    }
+
+    var myHeaders = new Headers();
+    myHeaders.append(`Authorization`, `Bearer ${localStorage.apiKey}`);
+    myHeaders.append("Cookie", "csrf=1f2f45cf3e044886ad54d4bc5eca773d");
+
+    var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    redirect: 'follow'
+    };
+
+    let url = `https://api.todoist.com/rest/v1/tasks/${localStorage.taskID}/close`
+
+    fetch(url, requestOptions)
+    .then(response => response.text())
+    .then(result => location = location)
+    .catch(error => console.log('error', error));
 
 }
